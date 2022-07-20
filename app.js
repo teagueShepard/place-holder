9697483ad44c51fff5ac6061f8e7cf7d84c4127f
teagueShepard <109492830+teagueShepard@users.noreply.github.com>
2022-07-19 23:16:46 -0700

    worked on start page

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
 console.log("Hello world")
 
-let introField = document.querySelector(".intro-field")
-let button = document.querySelector("button")
-
-button.onclick = function(){
-    let intro = introField.value
-    let message = "the solution to all your probems is " + intro
-    console.log(message)
-    document.write(message)
-}
