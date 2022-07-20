c312267859158982734e371ed476ed57762e003c
teagueShepard <109492830+teagueShepard@users.noreply.github.com>
2022-07-20 13:58:41 -0700

    worked on intro page

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 console.log("Hello world")
 
+//declaring variables
+
+    let usernameInput = document.querySelector(".usernameInput")
+    let usernameButton = document.querySelector(".usernameButton")
+    let usernameOutput = document.querySelector(".usernameOutput")
+    
+
+//set username function     
+
+        usernameButton.onclick = function (){
+            usernameOutput.textContent = "welcome:" + usernameInput.value
+        }
+
+//change username
+       usernameInput.onchange = function(){myScript};
+
+       
+
+    // let singlePlayerButton = document.querySelector(".singlePlayerButton")
+    // let multiPlayerButton = document.querySelector(".multiPlayerButton")
+
+    //     onclick
